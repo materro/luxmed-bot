89a9e8d01e14a870ba3745c2b527a50a8c44b43a
materro <materro+github@outlook.it>
2025-11-09 12:28:04 +0100

    api: Update headers
    * Fix User-Agent duplication
    * Clean up unused headers

--- api/src/main/scala/com/lbs/api/ApiBase.scala
+++ api/src/main/scala/com/lbs/api/ApiBase.scala
@@ -6,11 +6,13 @@ import scalaj.http.{BaseHttp, HttpRequest}
 
 import java.net.HttpCookie
 
-object ApiHttp
-  extends BaseHttp(
-    userAgent =
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
-  )
+object ApiHttpOld extends BaseHttp(
+  userAgent = "okhttp/4.9.1"
+)
+
+object ApiHttpNew extends BaseHttp(
+  userAgent = "Mozilla/5.0 (Linux; Android 14; Pixel 8 Build/AP2A.240905.003.D1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/113.0.5672.136 Mobile Safari/537.36"
+)
 
 trait ApiBase {
   private val uuid = java.util.UUID.randomUUID.toString.toUpperCase
@@ -25,40 +27,35 @@ trait ApiBase {
 
   private val OldApiHeaders =
     Map(
-      `X-Api-Client-Identifier` -> "iPhone",
-      `Custom-User-Agent` -> s"PatientPortal; 4.45.0; $uuid; iOS; 18.0; iPhone16,2",
-      `User-Agent` -> "Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
-    )
-
-  private val NewApiHeaders =
-    Map(
-      `Custom-User-Agent` -> s"PatientPortal; 4.45.0; $uuid; iOS; 18.0; iPhone16,2",
-      `User-Agent` -> "Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
+      `X-Api-Client-Identifier` -> "Android",
+      `Custom-User-Agent` -> s"Portal Pacjenta; 4.45.0; $uuid; Android; 34; google Pixel 8",
+      `User-Agent` -> "okhttp/4.9.1"
     )
 
   protected def httpUnauthorized(url: String): HttpRequest = {
-    ApiHttp(s"https://portalpacjenta.luxmed.pl/PatientPortalMobileAPI/api/$url")
+    ApiHttpOld(s"https://portalpacjenta.luxmed.pl/PatientPortalMobileAPI/api/$url")
       .headers(CommonHeaders ++ OldApiHeaders)
   }
 
   protected def http(url: String, session: Session): HttpRequest = {
-    ApiHttp(s"https://portalpacjenta.luxmed.pl/PatientPortalMobileAPI/api/$url")
+    ApiHttpOld(s"https://portalpacjenta.luxmed.pl/PatientPortalMobileAPI/api/$url")
       .headers(CommonHeaders ++ OldApiHeaders)
       .cookies(session.cookies)
       .header(Authorization, s"${session.tokenType} ${session.accessToken}")
   }
 
   protected def httpNewApi(url: String, session: Session, cookiesMaybe: Option[Seq[HttpCookie]] = None): HttpRequest = {
-    val req = ApiHttp(s"https://portalpacjenta.luxmed.pl/PatientPortal/$url")
-      .headers(CommonHeaders ++ NewApiHeaders)
+    val req = ApiHttpNew(s"https://portalpacjenta.luxmed.pl/PatientPortal/$url")
+      .headers(CommonHeaders)
       .header(AuthorizationToken, s"Bearer ${session.jwtToken}")
     cookiesMaybe.map(cookies => req.cookies(cookies)).getOrElse(req.cookies(session.cookies))
   }
 
   protected def httpNewApiWithOldToken(url: String, session: Session, cookiesMaybe: Option[Seq[HttpCookie]] = None): HttpRequest = {
-    val req = ApiHttp(s"https://portalpacjenta.luxmed.pl/PatientPortal/$url")
-      .headers(CommonHeaders ++ NewApiHeaders)
+    val req = ApiHttpNew(s"https://portalpacjenta.luxmed.pl/PatientPortal/$url")
+      .headers(CommonHeaders)
       .header(`X-Requested-With`, "pl.luxmed.pp")
+      .header("Upgrade-Insecure-Requests", "1")
       .header(Authorization, session.accessToken)
     cookiesMaybe.map(cookies => req.cookies(cookies)).getOrElse(req.cookies(session.cookies))
   }
